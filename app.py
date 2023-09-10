2dbf45f4f0be60dd1f0d5c32de408d81ee187b9c
FireHawk <usama@networkconfig.net>
2023-09-11 02:19:59 +0500

    Uploaded python project

---
+++ app.py
@@ -0,0 +1,10 @@
+import http.server
+import socketserver
+
+PORT = 8099
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving on port {PORT}")
+    httpd.serve_forever()
